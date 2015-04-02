5c22e730259dcf10da8b5ffb86e47cad0e678b8a
jamc92 <javiermadrid19@hotmail.com>
2015-04-02 15:57:27 -0430

    Added subscription to a publications

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
 /**
  * Created by madrid on 15/03/15.
  */
+//Subscribing to posts elements in Post collection
+Meteor.subscribe('posts');
---
+++ server/publications.js
@@ -0,0 +1,11 @@
+Template.publications.created = function () {
+    //add your statement here 
+};
+
+Template.publications.helpers({
+    //add you helpers here
+});
+
+Template.publications.events({
+    //add your events here
+});
