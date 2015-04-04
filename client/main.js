00c24447ef36bff384d8502292d9c10a77c5d585
jamc92 <javiermadrid19@hotmail.com>
2015-04-04 17:49:54 -0430

    Added cfg for cpal:loading

--- client/main.js
+++ client/main.js
@@ -1,5 +1,26 @@
 /**
  * Created by madrid on 15/03/15.
  */
-//Subscribing to posts elements in Post collection
-Meteor.subscribe('posts');
+
+// < START Config for package pcel:loading
+Template.loading.rendered = function () {
+    if ( ! Session.get('loadingSplash') ) {
+        this.loading = window.pleaseWait({
+            logo: '/images/mowebjobs.png',
+            backgroundColor: '#40d47e',
+            loadingHtml: message + spinner
+        });
+        Session.set('loadingSplash', true); // just show loading splash once
+    }
+};
+
+Template.loading.destroyed = function () {
+    if ( this.loading ) {
+        this.loading.finish();
+    }
+};
+
+var message = '<p class="loading-message">MowebJobs</p>';
+var spinner = '<div class="sk-spinner sk-spinner-rotating-plane"></div>';
+
+// END Config for package pcel:loading >
